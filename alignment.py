f948f0a76dabeab42701259a1be29dbb08681ca4
Angela Yang <31804752+angelagyang@users.noreply.github.com>
2020-07-29 13:33:47 -0400

    Update alignment.py

--- alignment.py
+++ alignment.py
@@ -2,6 +2,9 @@ import csv
 from collections import defaultdict
 
 class Alignment:
+    """
+    Reads the csv into a dictionary 
+    """
     def __init__(self, filename="gloss_alignment.csv"):
         self.alignment_dict = self.csv_to_dict(filename)
      
@@ -14,6 +17,20 @@ class Alignment:
                     alignment_dict[k].append(v)
         return alignment_dict
     
-    def find_match(self, word): 
-        return self.alignment_dict['Pose2Idx'][self.alignment_dict['Eng2GlossVocab'].index(word)] 
-                                        
+    """
+    For a single gloss token (output from eng2gloss), returns the index of corresponding gloss token in pose2gloss vocab
+    - index used to lookup the corresponding symbol/video in Liz.data
+    """
+    def find_match(self, token): 
+        if word in self.alignment_dict['Eng2GlossVocab']:
+            return self.alignment_dict['Pose2Idx'][self.alignment_dict['Eng2GlossVocab'].index(token)] 
+        else: 
+            return 0 # shouldn't happen 
+        
+"""
+Example usage 
+a = Alignment()
+print(a.alignment_dict.keys())
+print(a.find_match("A")) --- 11
+print(pose2idx['A']) ------- 11
+"""
